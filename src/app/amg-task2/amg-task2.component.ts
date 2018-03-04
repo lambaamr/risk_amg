f6f3f0cea2de499a32e9e62582977a890137bf4a
lambaamr <32879209+lambaamr@users.noreply.github.com>
2018-03-04 14:57:34 -0500

    change

--- src/app/amg-task2/amg-task2.component.ts
+++ src/app/amg-task2/amg-task2.component.ts
@@ -34,10 +34,10 @@ export class AmgTask2Component implements OnDestroy {
               .takeWhile(() => this.active)
               .subscribe(res => {
                 this.amgtask = res.json();
-                this.maxPage = this.amgtask.length -1;
-                this.pages = this.shuffle(this.maxPage );
+                this.maxPage = this.amgtask.length -2;
+                this.pages = this.shuffle(this.maxPage);
                 this.curParticipantService.pages = this.pages.slice().reverse();
-                this.keyPresses = new Array(7);
+                this.keyPresses = [];
               });
   }
 
